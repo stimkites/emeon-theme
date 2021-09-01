c213a72de660a164db77b3c7b929f8e6eefb6117
evgeni83 <evgeni.daniluk@yandex.ru>
2021-09-01 15:50:01 +0300

    removed unused code from theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,62 +1,51 @@
-(function(){
+/* Theme's JS that fires on all pages */
 
-})()
+window.onload = ( function( $ ) {
 
-jQuery(document).ready(function($){
-	$("li.gallery-image").click(function(){
-		$("li.gallery-image").toggleClass("zoom-image");
-	});
-});
-jQuery(document).ready(function($){
-	jQuery('.menu-item.cart').click(function () {
-    	jQuery('.woocommerce.widget_shopping_cart').toggleClass('open');
-// 		jQuery('button.menu-toggle').toggleClass('close');
-    });
-});
-
-// Toogle WC filters
-jQuery(document).ready(function($){
-	jQuery('.wc-filters').click(function () {
-		jQuery('.wc-filters').toggleClass('active');
-    	jQuery('.woocommerce-widgets').toggleClass('active');
-// 		jQuery('button.menu-toggle').toggleClass('close');
-    });
-});
-
-// Remove Parenthasis () from WooCommerce filter count
-jQuery( function($) {
-$('.woocommerce-widget-layered-nav-list .count').each( function() {
-	$(this).html( /(\d+)/g.exec( $(this).html() )[0] );
-	} );
-} );
-
-// Toogle nav menu
-jQuery(document).ready(function($){
-	jQuery('button.toggle-mobile-menu').click(function () {
-		jQuery('button.toggle-mobile-menu').toggleClass('open');
-		jQuery('.menu-header-menu-container').toggleClass('open');
-    });
-});
-
-// Toogle search
-jQuery(document).ready(function($){
-	jQuery('.toggle-search').click(function () {
-		jQuery('.main-search').toggleClass('open');
-    });
-});
-
-jQuery(document).ready(function($){
-	function display_logo_handler() {
+	/**
+	 * Change logo on scroll
+	 *
+	 * @private
+	 */
+	function __display_logo_handler() {
 		if ( window.pageYOffset > 0 ) {
-			jQuery('.site-branding, .inline-branding .slogan').hide();
-			jQuery('.inline-branding .logo-wrap').fadeIn(200);
+			$( '.site-branding, .inline-branding .slogan' ).hide();
+			$( '.inline-branding .logo-wrap' ).fadeIn( 200 );
 		} else {
-			jQuery('.site-branding, .inline-branding .slogan').fadeIn(200);
-			jQuery('.inline-branding .logo-wrap').hide();
+			$( '.site-branding, .inline-branding .slogan' ).fadeIn( 200 );
+			$( '.inline-branding .logo-wrap' ).hide();
 		}
 	}
 
-	display_logo_handler();
 
-	jQuery( window ).on( 'scroll', display_logo_handler );
-});
+	/**
+	 * Assign events handlers
+	 *
+	 * @private
+	 */
+	function __assign() {
+		$( window ).on( 'scroll', __display_logo_handler );
+	}
+
+
+	/**
+	 * Initialize
+	 *
+	 * @private
+	 */
+	function __init() {
+		console.info( '[theme] Theme JS initiated!' );
+		__display_logo_handler();
+		__assign();
+	}
+
+
+	return {
+		/**
+		 * Call to init ourselves only when we are on our archive page
+		 */
+		init: function() {
+			return __init;
+		}
+	}
+} )( jQuery.noConflict() ).init();
