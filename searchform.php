ebfefd71d1efe34865c48d5fa38e0a1f45f693a1
Stim <stim@wetail.se>
2021-10-28 19:39:41 +0300

    Ongoing filters

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
 
 $_id = rand();
 ?>
-<form action="/?" id="search-form-<?= $_id ?>" method="post" enctype="application/x-www-form-urlencoded" class="search-filters">
+<form action="/" id="search-form-<?= $_id ?>" method="GET" enctype="application/x-www-form-urlencoded" class="search-filters">
 	<fieldset class="search-field">
 		<select class="search emeon-search-select"
 		        id="search-select-<?= $_id ?>"
