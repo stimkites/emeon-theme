098b642c58e067f4bd4801484c4adfb5cfd28433
evgeni83 <evgeni.daniluk@yandex.ru>
2021-11-01 16:54:04 +0300

    "for" word removed from search page title

--- search.php
+++ search.php
@@ -21,7 +21,7 @@ get_header();
 					<h1 class="page-title">
 						<?php
 						/* translators: %s: search query. */
-						printf( esc_html__( 'Search Results for: %s', 'emeon' ), '<span>' . get_search_query() . '</span>' );
+						printf( esc_html__( 'Search Results: %s', 'emeon' ), '<span>' . get_search_query() . '</span>' );
 						?>
 					</h1>
 				</header><!-- .page-header -->
