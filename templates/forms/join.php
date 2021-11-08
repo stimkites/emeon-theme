d9713345ac84c07e582e8ad26ea06d9e4538348d
stim <stim@wetail.se>
2021-11-08 19:08:24 +0300

    Ongoing "join" rework

--- templates/forms/join.php
+++ templates/forms/join.php
@@ -16,22 +16,24 @@ $nonce = wp_create_nonce( EMEON_SLUG );
 		<input type="hidden" name="__nonce" value="<?= $nonce ?>"/>
 
 		<div id="step-1" class="emeon-join step-1">
-			<p>Step 1: your email, please</p>
+			<p>Step 1: your email, please.</p>
+			<p class="hint">We will send you a password to login</p>
 			<div class="form-group">
 				<label for="join_email" data-valid="Email is not valid" data-empty="This field couldn't be empty" data-success="Password is sent!">
 					<input id="join_email" class="form-control email" name="email" type="email" placeholder="Email"/>
 				</label>
+				<button type="button" id="btn-join" class="button button-cta btn btn-primary block">Join</button>
 			</div>
-			<button type="button" id="btn-join" class="button button-cta btn btn-primary block">Join</button>
 		</div>
 		<div id="step-2" class="emeon-join step-2" style="display: none">
 			<p>Step 2: the password you received, please</p>
+			<p class="hint">Enter the password we sent (check "spam")</p>
 			<div class="form-group">
 				<label for="join_pass" data-valid="Password is not valid" data-empty="This field couldn't be empty" data-success="Welcome aboard!">
 					<input id="join_pass" class="form-control email" name="pass" type="password" placeholder="Password"/>
 				</label>
+				<button type="button" id="btn-login" class="button button-cta btn btn-primary block">Login</button>
 			</div>
-			<button type="button" id="btn-login" class="button button-cta btn btn-primary block">Login</button>
 		</div>
 	</form>
 </div>
