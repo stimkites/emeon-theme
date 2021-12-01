2ef4c58c5e71418e14c9bc12ec27fc61a5878f61
Stim <stim@wetail.se>
2021-12-01 09:21:56 +0300

    Merged to Alex, added checkbox on join for privacy policy

--- templates/forms/join.php
+++ templates/forms/join.php
@@ -17,15 +17,19 @@ $nonce = wp_create_nonce( EMEON_SLUG );
 		<div id="step-1" class="emeon-join step-1">
 			<p class="hint">We will send you a password to <a href="/account/">login</a></p>
 			<div class="form-group">
-				<label for="join_email" data-valid="Email is not valid" data-empty="This field couldn't be empty">
+				<label for="join_email" data-valid="Email is not valid" data-empty="This field cannot be empty">
 					<input id="join_email" class="form-control email" name="email" type="email" placeholder="Email"/>
 				</label>
-				<button type="submit" id="btn-join" class="button button-cta btn btn-primary block">Join</button>
+				<button type="submit" disabled id="btn-join" class="button button-cta btn btn-primary block">Join</button>
 			</div>
 		</div>
 	</form>
 	<p class="privacy-policy-link">
-		By joining us you accept and agree to our <a href="/privacy-policy/" target="_blank" title="Privacy policy">privacy policy rules.</a>
+        <label>
+            <input type="checkbox" name="accept_policy" id="accept-policy" value="yes" />
+		    By joining us you accept and agree to our
+            <a href="/privacy-policy/" target="_blank" title="Privacy policy">privacy policy rules.</a>
+        </label>
 	</p>
 </div>
 
