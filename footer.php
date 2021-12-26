465dd9e8dcbc6488cb71f868b6d34cc11efc24b2
stim <stim@wetail.se>
2021-12-26 16:03:02 +0300

    Ongoing footer rework

--- footer.php
+++ footer.php
@@ -17,12 +17,30 @@ defined( 'ABSPATH' ) or exit;
 
 <?php if( ! EMEON_PRINTABLE ) :?>
 
+
 <footer id="colophon" class="site-footer">
+	<div class="basic-info">
+		<div class="footer-logo">
+			<img width=100 height=100 src="<?=get_template_directory_uri()?>/img/emeon-logo.svg" alt="" />
+		</div>
+		<div class="footer-general-info">
+
+		</div>
+		<div class="footer-register">
+			<?=do_shortcode( '[emeon_forms form="join"]' )?>
+		</div>
+		<div class="footer-socials">
+
+		</div>
+	</div>
 	<div class="site-info">
 		<a href="https://emeon.io">
 			&copy; <?php echo get_bloginfo(); ?> 2020 - <?php echo date( 'Y' ); ?>
 		</a>
 		<span class="sep"> | </span>
+		<div class="half">
+			Works ♥ on WordPress
+		</div>
 		<a href="https://www.facebook.com/people/Emeon-Webdev/100075559053781/" target="_blank">
 			Facebook
 		</a>
--- js/theme.js
+++ js/theme.js
@@ -129,7 +129,7 @@ window.onload = ( function( $ ) {
 			autoHeight: true,
 			autoplay: {
 			 	disableOnInteraction: false,
-        delay: 1500,
+        		delay: 3500,
 			},
       navigation: {
         nextEl: '.swiper-button-next--projects',
@@ -154,7 +154,7 @@ window.onload = ( function( $ ) {
       autoHeight: true,
       autoplay: {
         disableOnInteraction: false,
-        delay: 4000,
+        delay: 8000,
       },
       pagination: {
         el: '.swiper-pagination--themes',
